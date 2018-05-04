f620a11e282ee46aaf53716312de01ec3fe32db3
Fan Wang <wangfan@mit.edu>
2018-05-04 15:05:53 -0400

    fix the dipole momentum derivative

--- src/CO.jl
+++ src/CO.jl
@@ -211,11 +211,11 @@ function CO(DefaultT=Float64;
     totalNU0 = CU * ntotal * f_3_0
     alpha_0 = exp(-log(2)*((f_pump-f₀)/Δ_f₀D)^2)*sqrt(log(2)/pi)/Δ_f₀D *
               8*pi^3/3/h/c * (totalNL0 - totalNU0) *
-              1e-36 * (0.2756^2*16.0/45) * f_pump * 1e-13
+              1e-36 * (0.01156806*max(JL, JU)/(2JL+1)) * f_pump * 1e-13
     alpha_r = alpha_0 * ones(num_layers)
     pump_IR = zeros(num_freq, num_layers)
     # in unit m^-3 microsec^-1
-    pump_0 = 9.4e13 * power/(radius^2)/Δ_f₀D * (0.2756^2*16.0/45) *
+    pump_0 = 9.4e13 * power/(radius^2)/Δ_f₀D * (0.01156806*max(JL, JU)/(2JL+1)) *
                 exp(-log(2)*((f_pump-f₀)/Δ_f₀D)^2)/norm_time
 
     # kwall = WallRate(radius, pressure, r_int, ntotal, M, T, NA, v_avg, σ_GKC) + 1e-10
