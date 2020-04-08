1be0f5f85135d83216d29e7eda61ca4373e06b9b
Fan Wang <wangfan@mit.edu>
2020-04-08 15:27:42 -0400

    modify three level model

--- src/three_level_model.jl
+++ src/three_level_model.jl
@@ -8,11 +8,20 @@ function μij2_THz(p)
 end
 
 function μij2_IR(p)
-    if p.M == 44
+    if p.M == 44 # n2o
         return 0.060373/2
     end
     if p.M == 28
         return 0.011568/2
+    end
+end
+
+function Δfp(p)
+    if p.M == 44
+        return 4.0e6*(p.pressure/1e3)
+    end
+    if p.M == 28
+        return 3.2e6*(p.pressure/1e3)
     end
 end
 
@@ -25,75 +34,106 @@ function kddrate(p)
 end
 
 function kSPTrate(p)
-    kspt = p.ka[1]*1e6 * 0
+    kspt = p.ka[1]*1e6*0
 end
 
-function IRabsorption(αIR, Pqcl, p)
+function IRabsorption(αIR, Pqcl, p; model="TLM", multitrip=true)
+    ## compute RHS (αIR) given αIR
     Lcell = p.L /100
     Rcell = p.radius/100
-    tot_ir_abs = total_IR_abs(Pqcl, p, αIRgiven=true, αIR=αIR)
+    tot_ir_abs = total_IR_abs(Pqcl, p, αIRgiven=true, αIR=αIR, multitrip=multitrip)
     Rpump = Pqcl * tot_ir_abs / (p.h*p.f₀) / (π*Rcell^2*Lcell)
     kw = wallrate(p.v_avg/sqrt(2), Rcell, p)
     kdd = kddrate(p)
     kspt = kSPTrate(p)
-    N2 = Rpump/(2kdd+kw+kspt) *(kdd+kw+kspt)/(kw+kspt)
-    N2pN3 = Rpump/(kw+kspt)
+    if model == "TLM"
+        N2 = Rpump/(2kdd+kw+kspt) *(kdd+kw+kspt)/(kw+kspt)
+    elseif model == "FLM"
+        N2 = Rpump/(1.5kdd+kw+kspt) *(kdd+kw+kspt)/(kw+kspt+0.5kdd)
+    elseif model == "FLM2"
+        N2 = Rpump * (kdd+kw+kspt)/((kdd+kw+kspt)^2 - 0.5kdd^2)
+    end
+    N1 = -Rpump/(kdd+kw)
     ΔN_IR0 = (p.CL * p.ntotal * p.f_G_0 - p.CU * p.ntotal * p.f_3_0)
+    # S_nu0 = sqrt(log(2)/π)/p.Δ_f₀D
+    S_nu = 1/π * 1/p.Δ_fP
+    S_nu0 = 1/π * 1/sqrt(p.Δ_f₀D^2 + p.Δ_fP^2)
     α = 8*π^3/3/p.h/p.c * 1e-36 * μij2_IR(p) * p.f₀ * 1e-13 *
-    (ΔN_IR0*sqrt(log(2)/π)/p.Δ_f₀D - (N2+N2pN3)*sqrt(log(2)/π)/p.Δ_f₀D) #/π)
+    (ΔN_IR0*S_nu0 + (N1-N2)*S_nu0) #
+    # α = 8*π^3/3/p.h/p.c * 1e-36 * μij2_IR(p) * p.f₀ * 1e-13 *
+    # (ΔN_IR0*sqrt(log(2)/π)/p.Δ_f₀D + (N1-N2)*sqrt(log(2)/π)/p.Δ_f₀D) #/π)
     return α
 end
 
-function compute_α(Pqcl, p)
+function compute_α(Pqcl, p; model="TLM", multitrip=true)
+    # compute αIR self consistently
     α = nlsolve((fvec, x) -> begin
-                        fvec[1] = IRabsorption(x[1], Pqcl, p) - x[1]
+                        fvec[1] = IRabsorption(x[1], Pqcl, p, model=model, multitrip=multitrip) - x[1]
             end, [0.0], iterations=100)
     return α.zero[1]
 end
 
-function total_IR_abs(Pqcl, p; αIRgiven=false, αIR=0)
+function total_IR_abs(Pqcl, p; αIRgiven=false, αIR=0, model="TLM", multitrip=true)
     Lcell = p.L/100
-    R1 = 0.95
+    if multitrip
+        R1 = 0.95
+    else
+        R1 = 0
+    end
     R2 = 0.96*R1
     if !αIRgiven
-        αIR = compute_α(Pqcl, p)
+        αIR = compute_α(Pqcl, p, model=model, multitrip=multitrip)
     end
-    beta = expm(-αIR*Lcell)
+    beta = exp(-αIR*Lcell)
     tot_ir_abs = (1-beta)*(1+R1*beta)/(1-R1*R2*beta^2)
 #     tot_ir_abs = 1-beta^2
     return tot_ir_abs
 end
 
-function compute_th(p; given_α_cell=false, α_given=0.3, th0=0., Tpinhole=0.016)
+function compute_th(p; given_α_cell=false, α_given=0.3, th0=0.1, Tpinhole=0.016, model="TLM")
     if given_α_cell
         α_cell = α_given
     else
         α_cell = OPFIR.ohmicloss(p, 'U', "TE01")  - log(1-Tpinhole)/2/(p.L/100)
     end
     solvePth = nlsolve((fvec, x) -> begin
-        kw = wallrate(p.v_avg/sqrt(2), p.radius/100, p)
-
-        kdd = kddrate(p) #19.8 * p.pressure * p.σ_DD/ sqrt(p.T*p.M)*1e3
-        kSPT = kSPTrate(p) # p.ka[1]*1e6 #* 0
-        muij2 = μij2_THz(p) * (3.3e-30)^2
-        fvec[1] = α_cell/(2/(3*p.h^2*8.85e-12) * muij2/(p.v_avg*(p.radius/100)^2*p.L/100) *
-            total_IR_abs(x[1], p) / p.f₀ / (2kdd+kw+kSPT)) - x[1]
+        # kw = wallrate(p.v_avg/sqrt(2), p.radius/100, p)
+        #
+        # kdd = kddrate(p) #19.8 * p.pressure * p.σ_DD/ sqrt(p.T*p.M)*1e3
+        # kSPT = kSPTrate(p) # p.ka[1]*1e6 #* 0
+        # muij2 = μij2_THz(p) * (3.3e-30)^2
+        #
+        fvec[1] = α_cell/gaincoeff(x[1], p, model=model) - 1.0
+        # fvec[1] = α_cell/(2/(3*p.h^2*8.85e-12) * muij2/(p.v_avg*(p.radius/100)^2*p.L/100) *
+        #     total_IR_abs(x[1], p, model=model) / p.f₀ / (tt*kdd+kw+kSPT)) - x[1]
             end, [th0], iterations=100)
     pth = solvePth.zero[1]
 end
 
-function gaincoeff(Pqcl, p)
+function gaincoeff(Pqcl, p; model="TLM")
     kw = wallrate(p.v_avg/sqrt(2), p.radius/100, p)
     kdd = kddrate(p) #19.8 * p.pressure * p.σ_DD/ sqrt(p.T*p.M)*1e3
-    kSPT = kSPTrate(p) #p.ka[1]*1e6 #* 0
+    kspt = kSPTrate(p) #p.ka[1]*1e6 #* 0
     muij2 = μij2_THz(p) * (3.3e-30)^2
-    γ_0 = 2/(3*p.h^2*8.85e-12) * muij2/(p.v_avg*(p.radius/100)^2*p.L/100) *
-            total_IR_abs(Pqcl, p) * Pqcl / p.f₀ / (2kdd+kw+kSPT)
+    if model == "TLM"
+        tt = 2.0kdd+kw+kspt
+    elseif model == "FLM"
+        tt = 1.5kdd+kw+kspt
+    elseif model == "FLM2"
+        tt = ((kdd+kw+kspt)^2 - 0.5kdd^2)/(0.5kdd+kw+kspt)
+    end
+
+    Δν = p.Δ_f₀D*p.f_dir_lasing/p.f₀ + Δfp(p)
+    λTHz = p.c/p.f_dir_lasing
+    γ_0 = 2/(3*p.h^2*8.85e-12) * muij2/(λTHz*Δν*(p.radius/100)^2*p.L/100) *
+            total_IR_abs(Pqcl, p, model=model) * Pqcl / p.f₀ / tt
+    # γ_0 = 2/(3*p.h^2*8.85e-12) * muij2/(p.v_avg*(p.radius/100)^2*p.L/100) *
+    #         total_IR_abs(Pqcl, p, model=model) * Pqcl / p.f₀ / (tt*kdd+kw+kSPT)
 end
 
-function PTHz(Pqcl, p; given_α_cell=false, α_given=0.3, Tpinhole = 0.016)
+function PTHz(Pqcl, p; given_α_cell=false, α_given=0.3, Tpinhole = 0.016, model="TLM")
     νTHz = p.f_dir_lasing
-    γ0 = gaincoeff(Pqcl, p)
+    γ0 = gaincoeff(Pqcl, p, model=model)
     if given_α_cell
         α_cell = α_given
     else
@@ -102,9 +142,20 @@ function PTHz(Pqcl, p; given_α_cell=false, α_given=0.3, Tpinhole = 0.016)
     kdd = kddrate(p)
     kw = wallrate(p.v_avg/sqrt(2), p.radius/100, p)
     kspt = kSPTrate(p)
-    ks = kdd + 0.5kspt + 0.5kw
-    tot_ir_abs = total_IR_abs(Pqcl, p)
+
+    tot_ir_abs = total_IR_abs(Pqcl, p, model=model)
     Rpump = Pqcl * tot_ir_abs / (p.h*p.f₀) / (π*(p.radius/100)^2*(p.L/100))
-    ΔN = Rpump/(2kdd + kspt + kw)
-    output_power= 0.5*(p.h*νTHz) * Tpinhole * (π*(p.radius/100)^2) *(γ0/α_cell-1) * ks*ΔN/γ0
+    if model == "TLM"
+        ΔN = Rpump/(2kdd + kspt + kw)
+        ks = kdd + 0.5kspt + 0.5kw
+    elseif model == "FLM"
+        ΔN = Rpump/(1.5kdd + kspt + kw)
+        ks = 0.75kdd + 0.5kspt + 0.5kw
+    elseif model == "FLM2"
+        ΔN = Rpump*(0.5kdd+kw+kspt)/((kdd+kw+kspt)^2 - 0.5kdd^2)
+        ks = ((kdd+kw+kspt)^2 - 0.5kdd^2)/(kdd+2(kw+kspt)-0.25kdd^2/(kdd+kw+kspt))
+    end
+    Δν = p.Δ_f₀D*p.f_dir_lasing/p.f₀ + Δfp(p)
+    σ = 2π*μij2_THz(p)* (3.3e-30)^2/(3p.h*8.85e-12*p.c/νTHz*Δν)
+    output_power= 0.5*(p.h*νTHz) * Tpinhole * (π*(p.radius/100)^2) *(γ0/α_cell-1) * ks/σ
 end
