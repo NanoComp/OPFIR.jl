f020cb5676ae06a4d6bc3bd56122fd7b49c3c486
Fan Wang <wangfan@mit.edu>
2020-06-02 07:09:25 -0400

    small tweak

--- src/OPFIR.jl
+++ src/OPFIR.jl
@@ -9,7 +9,7 @@ for f in ("parameters.jl", "main.jl",
           "anderson_accel.jl", "fixedpoint.jl",
           "compute_rhs.jl", "compute_row_col_val.jl",
           "gain.jl", "pump.jl", "Tv_model.jl", "check.jl",
-          "Sherman_Morrison.jl", "plotting.jl", "E_vib.jl")
+          "Sherman_Morrison.jl", "E_vib.jl")
     include(f)
 end
 
