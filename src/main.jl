57d8c570d789e2854d01027dac98366717e64877
Fan Wang <fwangmit@gmail.com>
2020-09-29 23:57:21 -0400

    correct doubly counted Ohmic loss

--- src/main.jl
+++ src/main.jl
@@ -242,7 +242,7 @@ end
 function cavityloss(p, llevel, cavitymode) # in m^-1
     Rback = 1.
     Rfront = (1-efftrans(cavitymode)) * Rback
-    alpha = 2 * ohmicloss(p, llevel, cavitymode) - log(Rfront*Rback)/(2p.L/100)
+    alpha = ohmicloss(p, llevel, cavitymode) - log(Rfront*Rback)/(2p.L/100)
     return alpha
 end
 
